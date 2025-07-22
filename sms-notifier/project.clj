71e791f0259a95f0a68be257b0b35997ba25f2eb
gjmmaster <gabriel.jmmaster@gmail.com>
2025-07-22 11:13:57 -0300

    adiciona dependencias postgreesql ao project.clj

--- sms-notifier/project.clj
+++ sms-notifier/project.clj
@@ -1,5 +1,5 @@
-(defproject sms-notifier "0.1.0-SNAPSHOT"
-  :description "SMS Notifier service"
+(defproject sms-notifier "0.2.0-SNAPSHOT" ; Versão atualizada para refletir a mudança
+  :description "SMS Notifier service with database persistence"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
@@ -7,7 +7,10 @@
                  [clj-http "3.12.3"]
                  [cheshire "5.11.0"]
                  [environ "1.2.0"]
-                 [http-kit "2.5.3"]]
+                 [http-kit "2.5.3"]
+                 ;; --- DEPENDÊNCIAS NOVAS ADICIONADAS AQUI ---
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.postgresql/postgresql "42.6.0"]]
   :main ^:skip-aot sms-notifier.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
