b4b61b7f9bf165df8ff1015e8ccfb296f6a0d4fc
gjmmaster <gabriel.jmmaster@gmail.com>
2025-08-04 14:35:39 -0300

    Update email.clj

--- sms-notifier/src/sms_notifier/channels/email.clj
+++ sms-notifier/src/sms_notifier/channels/email.clj
@@ -1,9 +1,22 @@
-;; src/sms_notifier/channels/email.clj
 (ns sms-notifier.channels.email
   (:require [sms-notifier.protocols :as p]
             [clj-http.client :as client]
             [cheshire.core :as json]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [sms-notifier.utils :as utils]))
+
+(defn- build-email-subject [template]
+  (str "Alerta de Mudança de Categoria: " (utils/clean-template-name (:elementName template))))
+
+(defn- build-email-body [template]
+  (str "<h1>Alerta de Mudança de Categoria!</h1>"
+       "<p><b>CANAL ATIVO:</b> " (:wabaId template) "</p>"
+       "<ul>"
+       "<li><b>Nome do Template:</b> " (:elementName template) "</li>"
+       "<li><b>Categoria Anterior:</b> " (:oldCategory template) "</li>"
+       "<li><b>Nova Categoria:</b> " (:category template) "</li>"
+       "</ul>"
+       "<p>Atenciosamente,<br>JM MASTER GROUP.</p>"))
 
 (defn- send-email-via-api [to-email to-name subject body external-id]
   (if-let [email-api-url (env :email-api-url "https://www.apiswagger.com.br/api/email/send_single_email_to_single_or_multiple_recipients")]
@@ -31,16 +44,13 @@
 
 (defrecord EmailChannel []
   p/NotificationChannel
-  (send! [this contact-info message-details]
+  (send! [this contact-info template]
     (let [email-addresses (let [e (:email contact-info)]
-                            (cond
-                              (coll? e) e
-                              (nil? e) []
-                              :else [e]))
+                            (cond (coll? e) e, (nil? e) [], :else [e]))
           contact-name    (:name contact-info)
-          subject         (:subject message-details)
-          body            (:body message-details)
-          template-id     (:template-id message-details)
+          subject         (build-email-subject template)
+          body            (build-email-body template)
+          template-id     (:id template)
           results         (atom [])]
       (if (empty? email-addresses)
         {:status 400 :body "Nenhum endereço de e-mail fornecido."}
@@ -49,7 +59,6 @@
             (println (str "  [Email Channel] Disparando envio para o e-mail: " email))
             (let [response (send-email-via-api email contact-name subject body template-id)]
               (swap! results conj response)))
-          ;; Return a summary: the first failure or a generic success
           (or (first (filter #(not= 200 (:status %)) @results))
               {:status 200 :body "Todos os emails foram enviados com sucesso."}))))))
 
